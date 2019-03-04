b5168e3623c0d6cc0bd68742841593c29ecdd78c
Marcus Hutchings <greywolf@nevermore.plus.com>
2019-03-04 17:17:27 +0000

    Removed PatrollingGuard class.

--- Source/TestingGrounds/Private/PatrollingGuard.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PatrollingGuard.h"
-#include "Public/PatrollingGuard.h"
-
-const TArray<AActor*> APatrollingGuard::GetPatrolPoints() const
-{
-	return PatrolPoints;
-}
