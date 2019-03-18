4d87daab1d556bf85adda3e1f0d42ad0da53cbf8
Marcus Hutchings <greywolf@nevermore.plus.com>
2019-03-18 20:33:34 +0000

    Refactored mannequin to use C++ instead of Blueprint

--- Source/TestingGrounds/Character/Mannequin.h
+++ Source/TestingGrounds/Character/Mannequin.h
@@ -11,19 +11,35 @@ class TESTINGGROUNDS_API AMannequin : public ACharacter
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this character's properties
-	AMannequin();
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* Mesh1P;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* FirstPersonCameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class AGun* FP_Gun;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
+	// Sets default values for this character's properties
+	AMannequin();
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		TSubclassOf<AGun> FP_GunBlueprint;
+
+	/** Fires a projectile. */
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+		void Fire();
 };
